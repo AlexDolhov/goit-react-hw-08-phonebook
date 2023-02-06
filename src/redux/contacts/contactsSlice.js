3de68d88f976fb36e98094755ac569f35ec9ed1e
Alex Dolhov <alexdolhov@gmail.com>
2023-02-07 01:43:19 +0200

    fix delete contactsSlice

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -14,7 +14,9 @@ const addContactSuccessReducer = (state, action) => {
 };
 
 const deleteContactSuccessReducer = (state, action) => {
-  const index = state.items.findIndex(contact => contact.id === action.payload);
+  const index = state.items.findIndex(
+    contact => contact.id === action.payload.id
+  );
   state.items.splice(index, 1);
 };
 
